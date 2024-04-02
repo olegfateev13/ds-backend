5762a88080744394f3cafcca12d465b5fa72b93f
Олег Фатеев <olegfateev13@gmail.com>
2024-04-03 00:10:58 +0300

    basic functionality

--- src/app.py
+++ src/app.py
@@ -3,6 +3,26 @@ from flask import Flask, request
 from models.plate_reader import PlateReader, InvalidImage
 import logging
 import io
+import requests
+
+
+def process_number_recognition(ids):
+    result = []
+    for image_id in ids:
+        if int(image_id) not in [10022, 9965]:
+            return [image_id], 'invalid image id', 400
+        try:
+            response = requests.get(f'http://178.154.220.122:7777/images/{image_id}', timeout=5)
+        except requests.exceptions.RequestException as e:
+            return [image_id], 'Bad Gateway', 502
+        im = io.BytesIO(response.content)
+        try:
+            res = plate_reader.read_text(im)
+            result.append(res)
+        except InvalidImage:
+            logging.error('invalid image')
+            return [image_id], 'invalid image', 400
+    return result, '', 200
 
 
 app = Flask(__name__)
@@ -31,20 +51,33 @@ def greeting():
 
 # <url>:8080/readPlateNumber : body <image bytes>
 # {"plate_number": "c180mv ..."}
-@app.route('/readPlateNumber', methods=['POST'])
-def read_plate_number():
-    im = request.get_data()
-    im = io.BytesIO(im)
-
-    try:
-        res = plate_reader.read_text(im)
-    except InvalidImage:
-        logging.error('invalid image')
-        return {'error': 'invalid image'}, 400
-
+@app.route('/readPlateNumber/<int:id>', methods=['POST'])
+def read_plate_number(id):
+    # image_id = request.json['plate_number']
+    res, err, code = process_number_recognition([id])
+    if err:
+        return {
+            "error": err
+        }, code
     return {
-        'plate_number': res,
-    }
+        'plate_number': res
+    }, code
+
+
+@app.route('/readPlateNumbers', methods=['POST'])
+def read_plate_numbers():
+    image_ids = request.json['plate_numbers']
+    res, err, code = process_number_recognition(image_ids)
+    if err:
+        return {
+            "error": err,
+            "context": {
+                "image_id": res[0]
+            }
+        }, code
+    return {
+        'plate_numbers': res
+    }, code
 
 
 if __name__ == '__main__':
